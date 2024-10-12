85bd46596a019879d95a4f2d881e53a2703555e1
Uriel Montes <17001633@alumnos.utleon.edu.mx>
2024-10-11 19:14:30 -0600

    Zodiaco

--- src/app/formulario/zodiaco/zodiaco.component.ts
+++ src/app/formulario/zodiaco/zodiaco.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-zodiaco',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './zodiaco.component.html',
   styleUrl: './zodiaco.component.css'
 })
